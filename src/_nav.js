6d382ddd2404e905de2f3998d2cbfeb7a4905902
DanielDO93 <danieldionisio1993@gmail.com>
2020-11-05 21:27:12 -0600

    Update _nav.js

--- src/_nav.js
+++ src/_nav.js
@@ -1,35 +1,31 @@
 
- export  default {
+const items = [
+  {
+    title: true,
+    name: 'Inicio',
+    class: 'text-center',
+  },
+  {
+    name: 'Objetivo',
+    icon: 'icon-tag',
+  },
+  {
+    name: 'Introducción',
+    icon: 'icon-puzzle',
+  },
+  {
+    name: 'Ventas',
+    icon: 'icon-puzzle',
+  },
+  {
+    name: 'Despedida',
+    icon: 'icon-puzzle',
+  },
+  {
+    name: 'Evaluación del curso',
+    icon: 'icon-tag',
+  },
+];
 
-  items : [
-    {
-      title: true,
-      name: 'Inicio',
-      class: 'text-center',
-    },
-    {
-      name: 'Objetivo',
-      icon: 'icon-tag',
-    },
-    {
-      name: 'Introducción',
-      icon: 'icon-puzzle',
-    },
-    {
-      name: 'Ventas',
-      icon: 'icon-puzzle',
-    },
-    {
-      name: 'Despedida',
-      icon: 'icon-puzzle',
-    },
-    {
-      name: 'Evaluación del curso',
-      icon: 'icon-tag',
-    },
-  ]
- }
- 
-
-
+export  default  items
 
